587f312dd82608926ac77fbbd8ab3ebc0627807a
Aryan Kapoor <103902793+JRDPropulsion@users.noreply.github.com>
2023-01-04 08:54:09 -0500

    Create Orientation.h

---
+++ src/Orientation.h
@@ -0,0 +1,21 @@
+/*
+  Orientation.h - Library for retrieving orientation.
+  Created by Aryan Kapoor, 12/4/22.
+  Released into the public domain.
+*/
+
+
+#ifndef Orientation_h
+#include "Arduino.h"
+
+
+// Defines Orientation class
+class Orientation
+{
+    public:
+        double quaternion_update(double gyro_x, double gyro_y, double gyro_z, double dt, double *px, double *py, double *pz);
+    
+    private:
+};
+
+#endif
